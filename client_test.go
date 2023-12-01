204ab3600c58f1759982ec185e9960963d27be6d
zplzpl <zpl77777777@hotmail.com>
2023-12-01 11:04:08 +0800

    up

--- client_test.go
+++ client_test.go
@@ -1,6 +1,8 @@
 package api2captcha
 
 import (
+	"context"
+	"log"
 	"testing"
 )
 
@@ -10,4 +12,15 @@ func TestClient_GetBalance(t *testing.T) {
 
 func TestClient_Funcaptcha(t *testing.T) {
 
+}
+
+func TestClient_ImageToText(t *testing.T) {
+
+	cli := NewClient("APIKEY")
+	rs, err := cli.ImageToText(context.TODO(), "Image Base64")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(rs.Text)
 }
